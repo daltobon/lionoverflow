08e499ebdec1cf885d678d1809fd468d01893480
daltobon <daniel18.tobon@gmail.com>
2018-05-07 16:59:05 -0500

    Add parameters to model question and helper for partial view

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,4 +10,11 @@
 #  user_id     :integer
 
 class Question < ApplicationRecord
+	belongs_to :user
+	has_many :answers, :dependent => :destroy
+	has_many :comments, as: :commentable, :dependent => :destroy
+	has_many :votes, as: :voteable, :dependent => :destroy
+
+	validates :title, presence: true
+	validates :description, presence: true
 end
